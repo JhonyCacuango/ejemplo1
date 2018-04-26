08773e183f4c34a4fe86a9f673849d62a6fee965
1-21-01-MD-06\ESFOT <ahilton.guaras@epn.edu.ec>
2018-04-26 10:58:03 -0500

    Modific

--- ejercicio1.py
+++ ejercicio1.py
@@ -14,13 +14,13 @@ print ('numero de cedula:1724809239')
 a=1+7+2+4+8+0+9+2+3+9
 print('la suma es:',a)
 
-<<<<<<< HEAD
+
 nombre=" Ahilton Guaras"
 mi_nombre=nombre.split()
 id=1+7+2+4+9+1+2+0+1+7
 print("Nombre: ",mi_nombre)
 print("ID: ", id)
-=======
+
 nombre = ['Marco Orquera']
 print (nombre)
 cedula = [1,7,2,6,1,1,4,7,7,8]
@@ -34,4 +34,4 @@ for p in cedula:
     
 print ('La suma de la cedula es: ' )
 print (y)
->>>>>>> 18aa8c7389780d79caec327d80a4a8dfa17408b8
+
