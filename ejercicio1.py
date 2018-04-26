18aa8c7389780d79caec327d80a4a8dfa17408b8
1-21-01-MD-12\ESFOT <marco.orquera@epn.edu.ec>
2018-04-26 10:51:25 -0500

    Edit Marco Orquera

--- ejercicio1.py
+++ ejercicio1.py
@@ -13,3 +13,17 @@ print('Jhony Javier Cacuango')
 print ('numero de cedula:1724809239')
 a=1+7+2+4+8+0+9+2+3+9
 print('la suma es:',a)
+
+nombre = ['Marco Orquera']
+print (nombre)
+cedula = [1,7,2,6,1,1,4,7,7,8]
+
+y = 0
+
+for p in cedula:
+    print(p) 
+    x = p 
+    y = y + x
+    
+print ('La suma de la cedula es: ' )
+print (y)
